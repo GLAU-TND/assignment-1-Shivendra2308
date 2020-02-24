f509f28c7504e64e9c2cabc8cad87b3c2242a9b8
Shivendra2308 <shivendra.singh_cs18@gla.ac.in>
2020-02-24 17:29:21 +0530

    Getter and setter added in MylinkedList

--- src/datastructures/MylinkedList.java
+++ src/datastructures/MylinkedList.java
@@ -1,4 +1,24 @@
 package datastructures;
 
-public class MylinkedList {
+import node.Node;
+
+public class MylinkedList<T extends Comparable<T>> {
+    private Node<T> iter;
+    private Node<T> head;
+
+    public Node<T> getIter() {
+        return iter;
+    }
+
+    public void setIter(Node<T> iter) {
+        this.iter = iter;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
 }
